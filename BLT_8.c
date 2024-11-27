3a47233fcaf602943a57633c049264cc137a14ac
Pham-anh-Hub <phamphuonganhcutee@gmail.com>
2024-11-27 09:03:55 +0700

    Update BLT_8.c

--- BLT_8.c
+++ BLT_8.c
@@ -1,18 +1,18 @@
 #include<stdio.h>
 int main(){
-	int money, month;
+	int money;
 	float interest; 
 	printf("Nhap vao tien gui ");
 	scanf("%d",&money);
-	printf("Nhap vao so thang gui ");
-	scanf("%d",&month);
 	printf("Nhap vao lai suat ");
 	scanf("%f",&interest);
+
+	float tienNhan=money*(1+interest*0.01)*(1+interest*0.01)*(1+interest*0.01);
+	printf("\nTien nhan duoc la trong 3 thang : %.3f VND",tienNhan);
 	
-	float tienLai= money*interest*0.01*month/12;
+	float tienLai=tienNhan-money;
 	printf("Tien lai la: %.3f VND ",tienLai);
-	float tienNhan=money+tienLai;
-	printf("\nTien nhan duoc la: %.3f VND",tienNhan);
+	
 	
 	return 0; 
 } 
